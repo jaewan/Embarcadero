947e9e881d6fb7fd750c85408ebf736a637ea3e7
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-03-14 19:18:18 -0600

    Add more to teardown script

--- teardown.sh
+++ teardown.sh
@@ -6,5 +6,9 @@
 rm -rf abseil-cpp
 # folly: must use sudo due to python cache permissions
 sudo rm -rf folly
+rm -rf grpc
+
+# Clean up build directory
+rm -rf build
 
 # TODO: cleanup CXL emulation
