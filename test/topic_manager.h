964eba59ba0c2236efaaecad143e0b664b2d4a95
Jaewan Hong <dominico1004@hanmail.net>
2024-03-25 01:18:26 +0000

    CXL publish with header

--- test/topic_manager.h
+++ test/topic_manager.h
@@ -19,27 +19,39 @@ using GetNewSegmentCallback = std::function<void*()>;
 class Topic{
 	public:
 		Topic(GetNewSegmentCallback get_new_segment_callback, 
-				void* TInode_addr, const char* topic_name, int broker_id);
+				void* TInode_addr, const char* topic_name, int broker_id,
+				void* segment_metadata);
 
 		// Delete copy contstructor and copy assignment operator
 		Topic(const Topic &) = delete;
 		Topic& operator=(const Topic &) = delete;
 
 		void PublishToCXL(void* message, size_t size);
+		bool GetMessageAddr(size_t &last_offset,
+												void* last_addr, void* messages, size_t &messages_size);
 
 	private:
 		const GetNewSegmentCallback get_new_segment_callback_;
 		std::string topic_name_;
 		int broker_id_;
 		struct TInode *tinode_;
+		struct MessageHeader *last_message_header_;
 		
-		//TInode cache
 		int logical_offset_;
 		int written_logical_offset_;
 		size_t remaining_size_;
 		void* log_addr_;
+		void* written_physical_addr_;
+		struct MessageHeader *prev_msg_header_;
+		//TODO(Jae) set this to nullptr if the sement is GCed
+		void* first_message_addr_;
 		std::set<int> writing_offsets_;
 		std::priority_queue<int, std::vector<int>, std::greater<int>> not_contigous_;
+
+		//TInode cache
+		void* ordered_offset_addr_;
+		void* segment_metadata_;
+		size_t ordered_offset_;
 
 		//absl::mutex mu_;
 		std::mutex mu_;
@@ -55,6 +67,8 @@ class TopicManager{
 		void CreateNewTopic(const char topic[32]);
 		void DeleteTopic(char topic[32]);
 		void PublishToCXL(char topic[32], void* message, size_t size);
+		bool GetMessageAddr(const char* topic, size_t &last_offset,
+												void* last_addr, void* messages, size_t &messages_size);
 
 	private:
 		int GetTopicIdx(char topic[32]){
