9098917aba9e4be288f53f11297731788096bea3
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-03-26 11:49:43 -0600

    Building successfully on RHEL vm

--- src/cmake/peer_grpc.cmake
+++ src/cmake/peer_grpc.cmake
@@ -15,7 +15,7 @@ else()
 endif()
 
 find_package(Protobuf REQUIRED)
-find_package(gRPC REQUIRED)
+#find_package(gRPC REQUIRED)
 
 # Proto file
 get_filename_component(peer_proto "protobuf/peer.proto" ABSOLUTE)
