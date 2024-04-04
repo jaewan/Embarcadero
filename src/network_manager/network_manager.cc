bcacaea44896ef43c46c17377cac30040a2b55a4
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-04-04 16:30:04 -0600

    builds - does not use grpc yet

--- src/network_manager/network_manager.cc
+++ src/network_manager/network_manager.cc
@@ -5,38 +5,47 @@
 
 namespace Embarcadero{
 
-NetworkManager::NetworkManager(size_t queueCapacity, int num_io_threads):
+NetworkManager::NetworkManager(size_t queueCapacity, int num_receive_threads, int num_ack_threads):
 						 requestQueue_(queueCapacity),
 						 ackQueue_(50),
-						 num_io_threads_(num_io_threads){
-	// Create Network I/O threads
-	threads_.emplace_back(&NetworkManager::MainThread, this);
-	threads_.emplace_back(&NetworkManager::AckThread, this);
-	threads_.emplace_back(&NetworkManager::AckThread, this);
-	for (int i=3; i< num_io_threads_; i++)
-		threads_.emplace_back(&NetworkManager::Network_io_thread, this);
+						 num_receive_threads_(num_receive_threads),
+						 num_ack_threads_(num_ack_threads) {
 
-	for (int i=0; i< NUM_BUFFERS; i++){
-		buffers_[i] = (char*)malloc(BUFFER_SIZE);
-		buffers_counters_[i] = 0;
+	// Start by creating threads to acknowledge when messages are done being processed
+	for (int i = 0; i < num_ack_threads; i++) {
+		threads_.emplace_back(&NetworkManager::AckThread, this);
 	}
 
+	// Create receive threads to process received gRPC messages
+	for (int i = 0; i < num_receive_threads; i++) {
+		threads_.emplace_back(&NetworkManager::ReceiveThread, this);
+	}
 
-	while(thread_count_.load() != num_io_threads_){}
-	std::cout << "[NetworkManager]: \tCreated" << std::endl;
+	// Wait for the threads to all start
+	while (thread_count_.load() != num_receive_threads + num_ack_threads) {}
+	std::cout << "[NetworkManager]: \tAll threads created!" << std::endl;
 }
 
-NetworkManager::~NetworkManager(){
+NetworkManager::~NetworkManager() {
+	// Notify threads we would like to stop
 	stop_threads_ = true;
+
+	// TODO: we need to stop the receivers before we stop the ack queues
+
+	// Write a nullopt to the ack queue, so the ack threads can finish flushing the queue
 	std::optional<struct NetworkRequest> sentinel = std::nullopt;
-	ackQueue_.blockingWrite(sentinel);
-	for (int i=3; i<num_io_threads_; i++)
-		requestQueue_.blockingWrite(sentinel);
-	for (int i=0; i< NUM_BUFFERS; i++){
-		free(buffers_[i]);
+	for (int i = 0; i < num_ack_threads_; i++) {
+		EnqueueAck(sentinel);
 	}
 
-	for(std::thread& thread : threads_){
+	/*
+	for (int i=0; i< NUM_BUFFERS; i++) {
+		free(buffers_[i]);
+	}
+	*/
+
+	// Wait for all threads to terminate
+	for(std::thread& thread : threads_) {
 		if(thread.joinable()){
 			thread.join();
 		}
@@ -45,10 +54,11 @@ NetworkManager::~NetworkManager(){
 }
 
 //Currently only for ack
-void NetworkManager::EnqueueRequest(struct NetworkRequest req){
+void NetworkManager::EnqueueAck(std::optional<struct NetworkRequest> req) {
 	ackQueue_.blockingWrite(req);
 }
 
+/*
 #define READ_SIZE 1024
 #define MSG_SIZE 1000000
 struct EmbarcaderoReq{
@@ -59,87 +69,36 @@ struct EmbarcaderoReq{
 };
 char JaeDebugBuf[1024];
 std::chrono::high_resolution_clock::time_point start;
-void NetworkManager::Network_io_thread(){
-	start = std::chrono::high_resolution_clock::now();
+*/
 
+void NetworkManager::ReceiveThread(){
+	std::cout << "[Network Manager]: \tStarting Receive I/O Thread" << std::endl;
 	thread_count_.fetch_add(1, std::memory_order_relaxed);
-	std::optional<struct NetworkRequest> optReq;
 
-	while(!stop_threads_){
-		requestQueue_.blockingRead(optReq);
-		if(!optReq.has_value()){
-			break;
-		}
-		const struct NetworkRequest &req = optReq.value();
-		switch(req.req_type){
-			case Receive:
-				//TODO(Jae) define if its publish or subscribe
-				{
-				int buffer_off = GetBuffer();
-				char* buf = buffers_[buffer_off];
-				ssize_t bytes_read = read(req.client_socket, buf, READ_SIZE);
-				struct EmbarcaderoReq *clientReq = (struct EmbarcaderoReq*)buf;
-				// Create publish request
-				struct PublishRequest pub_req;
-				pub_req.client_id = 0;
-				pub_req.client_order = clientReq->client_order;
-				pub_req.topic[0] = '0';
-				pub_req.acknowledge = true;
-				pub_req.payload_address = (uint8_t*)buf + sizeof(EmbarcaderoReq);
-				pub_req.counter = &buffers_counters_[buffer_off];
-				pub_req.size = clientReq->size;
-
-				cxl_manager_->EnqueueRequest(pub_req);
-				disk_manager_->EnqueueRequest(pub_req);
-
-				close(req.client_socket);
-				}
-				break;
-			case Send:
-				std::cout << "Send" << std::endl;
-				break;
-			case Test:
-				{
-				int buffer_off = GetBuffer();
-				char* buf = buffers_[buffer_off];
-				memcpy(buf, JaeDebugBuf, 1024);
-				// Create publish request
-				struct PublishRequest pub_req;
-				pub_req.client_id = 0;
-				pub_req.client_order = 0;
-				pub_req.topic[0] = '0';
-				pub_req.acknowledge = true;
-				pub_req.payload_address = (void*)buf; 
-				pub_req.counter = &buffers_counters_[buffer_off];
-				pub_req.size = 1024;
-
-				cxl_manager_->EnqueueRequest(pub_req);
-				disk_manager_->EnqueueRequest(pub_req);
-				}
-				break;
-		}
-	}
+	// TODO: Implement this
+	//cxl_manager_->EnqueueRequest(pub_req);
+	//disk_manager_->EnqueueRequest(pub_req);
 }
 
-void NetworkManager::AckThread(){
-	std::optional<struct NetworkRequest> optReq;
+void NetworkManager::AckThread() {
+	std::cout << "[Network Manager]: \tStarting Acknowledgement I/O Thread" << std::endl;
 	thread_count_.fetch_add(1, std::memory_order_relaxed);
 
-	while(!stop_threads_){
+	std::optional<struct NetworkRequest> optReq;
+	while(true) {
 		ackQueue_.blockingRead(optReq);
-		if(!optReq.has_value()){
-			break;
+		if(!optReq.has_value()) {
+			// This should means we are trying to shutdown threads
+			assert(stop_threads_ == true);
+			std::cout << "[Network Manager]: \tTerminating Acknoweldgement I/O Thread" << std::endl;
+			return;
 		}
-		static std::atomic<int> JaeDebugCount{0};
-		JaeDebugCount.fetch_add(1, std::memory_order_relaxed);
-		if(JaeDebugCount == MSG_SIZE){
-			auto end = std::chrono::high_resolution_clock::now();
-			auto dur = end - start;
-			std::cout<<std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << std::endl;
-		}
+
+		// TODO(erika): do something with the message
 	}
 }
 
+/*
 int NetworkManager::GetBuffer(){
 	static std::atomic<int> counter{0};
 	int off = counter.fetch_add(1, std::memory_order_relaxed) % NUM_BUFFERS;
@@ -151,39 +110,11 @@ int NetworkManager::GetBuffer(){
 		off = (off+1) % NUM_BUFFERS;
 	}
 }
-
-void NetworkManager::MainThread(){
-	thread_count_.fetch_add(1, std::memory_order_relaxed);
-
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-	
-	 while (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        std::cerr << "Error binding socket" << std::endl;
-		sleep(5);
-    }
-
-
-    listen(server_socket, 32);
-
-    while (true) {
-		struct NetworkRequest req;
-		req.req_type = Receive;
-        req.client_socket = accept(server_socket, nullptr, nullptr);
-        if (req.client_socket < 0) {
-            std::cerr << "Error accepting connection" << std::endl;
-            continue;
-        }
-		//EnqueueRequest(req);
-		requestQueue_.blockingWrite(req);
-    }
-}
+*/
 
 } // End of namespace Embarcadero
 
+/*
 NetworkManager::NetworkManager(size_t num_net_threads) {
     num_net_threads_ = num_net_threads;
 }
@@ -209,3 +140,4 @@ void NetworkManager::Run(uint16_t port) {
     // Proceed to the server's main loop.
     HandleRpcs();
 }
+*/
