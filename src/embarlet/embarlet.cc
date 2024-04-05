db53727ba5f7f1d34645a903fa4df004d76cb048
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-04-05 10:04:53 -0600

    Remove unused classes

--- src/embarlet/embarlet.cc
+++ src/embarlet/embarlet.cc
@@ -1,6 +1,4 @@
 #include "common/config.h"
-#include "pub_queue.h"
-#include "pub_task.h"
 #include "peer.h"
 #include "topic_manager.h"
 #include "../disk_manager/disk_manager.h"
@@ -48,16 +46,6 @@ int main(int argc, char* argv[]){
 	memset(topic, 0, 32);
 	topic[0] = '0';
 	topic_manager.CreateNewTopic(topic);
-	
-	/*
-	Embarcadero::PublishRequest req;
-	std::atomic<int> c{2};
-	memcpy(req.topic, topic, 32);
-	req.payload_address = malloc(1024);
-	req.counter = &c;
-	req.size = 1024;
-	req.acknowledge = true;
-	*/
 
 	/*
 	Embarcadero::NetworkRequest req;
@@ -107,36 +95,6 @@ int main(int argc, char* argv[]){
 	} else {
 		std::cout << "Invalid arguments" << std::endl;
 	}
-
-	// Create a pub task
-	// This is slightly less than ideal, because it involved two heap allocations.
-	// But we'll worry about performance optimizations later.
-	std::atomic<uint8_t> *num_subtasks = new std::atomic<uint8_t>(1); // this will be freed by the PubTask destructor
-	PubTask *pt = new PubTask(num_subtasks);
-
-	// Create a pub queue
-	PubQueue *pq = new PubQueue(PUB_QUEUE_CAPACITY);
-
-	pq_enqueue(pq, pt);
-	pq_dequeue(pq, &pt);
-
-	uint8_t tasks_left = pt->subtasks_left->fetch_sub(1, std::memory_order_seq_cst);
-	printf("Tasks left: %d\n", tasks_left);
-	assert(tasks_left == 1);
-
-	pq_enqueue(pq, pt);
-	pq_dequeue(pq, &pt);
-
-	tasks_left = pt->subtasks_left->fetch_sub(1, std::memory_order_seq_cst);
-	printf("Tasks left: %d\n", tasks_left);
-	if (0 == tasks_left) {
-		printf("No tasks left! Time to signal the completion queue before freeing the PubTask!\n");
-		// TODO: signal completion queue
-		delete pt;
-	}
-	assert(tasks_left == 0);
-
-	delete pq;
 	*/
 
 	/*
