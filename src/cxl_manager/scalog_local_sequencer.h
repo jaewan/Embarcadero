e6ab554637581b61274a12d2ca9087b3c302c1e6
Jaewan Hong <dominico1004@hanmail.net>
2025-04-10 09:13:18 +0900

    [SCALOG] scalog sequencer IP moved to config

--- src/cxl_manager/scalog_local_sequencer.h
+++ src/cxl_manager/scalog_local_sequencer.h
@@ -48,7 +48,7 @@ class ScalogLocalSequencer {
 		int local_epoch_ = 0;
 
 		// Global seq ip
-		std::string scalog_global_sequencer_ip_ = "128.110.219.89";
+		std::string scalog_global_sequencer_ip_ = SCLAOG_SEQUENCER_IP;
 
 		/// Flag to indicate if we should stop reading from the stream
 		bool stop_reading_from_stream_ = false;
